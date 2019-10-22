2ec6319c905c3b607916a554b7546472717b107f
isabelsantana <50423863+isabelsantana@users.noreply.github.com>
2019-10-22 19:33:59 -0300

    Add files via upload

---
+++ Nome.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto_AED
+{
+    class Nome{
+        public int getNome(){
+            return Nome
+
+        }
+
+    }
+}
