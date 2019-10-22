fe977322bba69e9903e8047e671efdeb5c305f33
isabelsantana <50423863+isabelsantana@users.noreply.github.com>
2019-10-22 19:34:28 -0300

    Add files via upload

---
+++ Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto_AED
+{
+    class Animal{
+        private string nome;
+        private string raça;
+        private string habitat;
+        
+        public Nome GetNome(Nome nm)
+
+    
+    }
+}
