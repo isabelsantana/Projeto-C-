652fce27c8dd1c1c1bd758b98a85f65860df7027
PietraLopes <50422179+PietraLopes@users.noreply.github.com>
2019-12-05 21:21:20 -0300

    Create CadastroUsuario.cs

---
+++ CadastroUsuario.cs
@@ -0,0 +1,30 @@
+class CadastroUsuario{
+  private string nomeUsuario;
+  private int idade;
+  private double cpf;
+
+  public string getNomeUsuario(){
+    return nomeUsuario;
+  }
+  
+}
+  public int getIdade(){
+    return idade;
+}
+
+  public double getCpf(){
+    return cpf;
+  }
+
+Console.WriteLine("Digite seu nome completo:", nomeUsuario)
+string nomeUsuario = Console.ReadLine();
+Console.WriteLine("Digite sua idade:", idade)
+int idade = Console.ReadLine();
+Console.WriteLine("Digite seu cpf:", cpf)
+double cpf = Console.ReadLine();
+
+
+//string nomes = Console.ReadLine();
+            Cadastro.cadastrarCachorro(nomes);
+    string estados = Console.ReadLine();
+            Zoológico.cadastrarCachorro(estados);  
