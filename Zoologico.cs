1643b41a34d26a3e8daf3e3997d4a1440f7b9f1b
isabelsantana <50423863+isabelsantana@users.noreply.github.com>
2019-12-05 21:15:51 -0300

    Update Zoologico.cs

--- Zoologico.cs
+++ Zoologico.cs
@@ -1,25 +1,23 @@
-﻿class Zoológico
-{
-    public string nomeEstado;
-    public string nomeZoologico;
+class Zoológico{
+  private string nomeEstado;
+  private string nomeZoologico;
+  //private Animal[] animais_zoo;
 
-    public string getNomeEstado()
-    {
-        return nomeEstado;
-    }
-
-    public string getNomeZoologico()
-    {
-        return nomeZoologico;
-    }
-
-    public string (){
-    string[] nomeEstado = new string[] { "São Paulo", "Belo Horizonte", "Brasília", "Belém", "Bahia", "Recife", "Santa Catarina", "Paraná" };
+  public string getNomeEstado(){
     return nomeEstado;
   }
 
-public string (){
-    string[] nomeZoologico = new string[] { "Zoológico de São Paulo", "Zoológico de Belo Horizonte", "Zoológico de Brasília", "Parque Zoobotânico do Museu Paraense Emílio Goeldi", "Zoológico de Salvador", "Parque Dois Irmãos", "Parque das Aves", "Zoo Pomorode" };
+  public string getNomeZoologico(){
+    return nomeZoologico;
+  }
+
+  public string[] getEstados(){
+    string[] nomeEstado = new string[] {"São Paulo", "Belo Horizonte", "Brasília", "Belém", "Bahia", "Recife", "Santa Catarina","Paraná"};
+    return nomeEstado;
+  }
+
+  public string[] getZoologicos(){
+    string[] nomeZoologico = new string[] {"Zoológico de São Paulo", "Zoológico de Belo Horizonte", "Zoológico de Brasília", "Parque Zoobotânico do Museu Paraense Emílio Goeldi", "Zoológico de Salvador", "Parque Dois Irmãos", "Parque das Aves","Zoo Pomorode"};
     return nomeZoologico;
   }
 }
