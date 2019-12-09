aa9cd7809f763003782b6aedd374357e54214546
PietraLopes <50422179+PietraLopes@users.noreply.github.com>
2019-12-09 04:05:03 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -39,12 +39,23 @@ class Mainclass{
       }
       else{
         if(resposta == "não"){
-          Sumario match = new Sumario( cadastradoSumario, Animais.retornaVetoranimais());
-       
+          string[] animais = new string[10] { "ANIMAIS","1.Hipopotamo", "2.Leão", "3.Lobo Guará", 
+          "4.Cacatua", "5.Papagaio", "6.Guepardo", "7.Coala", "8.Mico Leão Dourado",
+          "9.Crocodilo" };
 
-
-        }
+      foreach (string animal in animais){
+        Console.WriteLine(animal);
       }
+         string[] estados = new string[9] {"ESTADOS","1.São Paulo", "2.Belo Horizonte", "3.Brasília", "4.Belém", "5.Bahia", "6.Recife", "7.Santa Catarina","8.Paraná"};
+
+      foreach (string estado in estados){
+        Console.WriteLine(estado);
+      }
+      
+        
+    }
+
+    }  
     
   }
 
